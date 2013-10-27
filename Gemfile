d7a61c311cf28b82609a3da6bb6aacd3f560aef0
Rob Pennock <robpennock@gmail.com>
2013-10-27 16:54:13 -0700

    Got calendar month and year selection working. Issue with timepicker pluggin after page modification

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ group :production do
 	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
 end
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
 
 # Bootstrap styling
 gem 'bootstrap-sass', '2.3.2.0'
@@ -31,9 +33,6 @@ gem 'coffee-rails', '~> 4.0.0'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,8 @@
-var currentDay;
+var currentDay;		//globals
 var currentYear;
 var currentMonth;
 var firstWeekFlag;
+var d = new Date();
 
 var months = [
 "January",
@@ -18,50 +19,99 @@ var months = [
 "December"
 ];
 
+//sets the date object with current month, day, and year
+function initializeCurrentDate(){
+	currentDay=d.getDay();
+	currentYear=d.getFullYear();
+	currentMonth=d.getMonth();
+}
 // returns a number 0-11 representing the current month
 function getMonth(){
-	var d = new Date();
-	return d.getMonth();
+	//var d = new Date();
+	return currentMonth;
 }
 
 // returns a string representing the current month's name
 function getMonthName(){
 	return months[getMonth()];
 }
-
+//gets the 4 digits of the year
 function getYear(){
-	var d = new Date();
-	return d.getFullYear();	
+	//var d = new Date();
+	return currentYear;	
 }
-
+//return integer day of the week 0-6
 function getDay(){
-	var d = new Date();		
+	//var d = new Date();		
 	return d.getDay();
 }
-
+//return the current day of month 1-31
 function getDate(){
-	var d = new Date();		
+	//var d = new Date();		
 	return d.getDate();
 }
-
+//get the first day of the month
 function getFirstDay(){
-	var d = new Date();
-	var year = d.getFullYear();
-	var month = d.getMonth();	
+	//var d = new Date();
+	var year = currentYear;
+	var month = currentMonth;	
 	var date = new Date(year, month, 1);
 	return date.getDay();
 }
 
+
+//get the last day of the month
 function getLastDay(){
-	var d = new Date();
-	var year = d.getFullYear();
-	var month = d.getMonth();	
+	//var d = new Date();
+	var year = currentYear;
+	var month = currentMonth;	
 	var date = new Date(year, month + 1, 0);
 	return date.getDate();
 }
-
+//set firstWeekFlag as needed
+function setFirstWeekFlag(tOrF){
+	if(tOrF==true)
+		firstWeekFlag=true;
+	else
+		firstWeekFlag=false;
+}
+function incrementMonth(){
+	currentMonth++;
+    if(currentMonth<12)
+    	d.setMonth(currentMonth);
+    else{
+    	currentMonth=0;
+    	incrementYear();
+    	d.setMonth(currentMonth);
+    }
+    setFirstWeekFlag(true);
+}
+function decrementMonth(){
+	currentMonth--;
+    if(currentMonth<0){
+    	currentMonth=11;
+    	decrementYear();
+    	d.setMonth(currentMonth);
+    }	
+    else{
+    	d.setMonth(currentMonth);
+    }
+    setFirstWeekFlag(true);
+}
+function incrementYear(){
+	currentYear++;
+	d.setYear(currentYear);
+    setFirstWeekFlag(true);
+}
+function decrementYear(){
+	currentYear--;
+	d.setYear(currentYear);
+    setFirstWeekFlag(true);
+}
+//this is the function that pretty much does everything
+//this creates a row of cells 1 week at a time
 function makeWeek(){
-	var week = "<tr>";
+	var week = "<tr>";	//our new string of tags to make week row
 	var numOfBlanks = 0;
 	if(firstWeekFlag){
 		//loop for initial blanks
@@ -69,7 +119,7 @@ function makeWeek(){
 			week = week + "<td> </td>";
 			numOfBlanks++;
 		};	
-		firstWeekFlag = false;
+		firstWeekFlag = false;	//remember to reset flag
 	}		
 	//given number of blanks before first day (weekDay), loop until end of week
 	for (var i = 0; i < (7 - numOfBlanks); i++) {				
@@ -92,10 +142,14 @@ function makeMonth(){
 		$('.calendar tbody').append(makeWeek());
 	};
 }	
+function clearMonth(){
+	$('tbody').empty();	//clear the tbody section which contains all calendar cells
+};
 
 //sourced from => http://www.stormconsultancy.co.uk/blog/development/code-snippets/jquery-document-ready-events-and-turbolinks/
 var do_on_load = function(){
 	//re-render the calendar on a page change
+	initializeCurrentDate();
 	makeMonth();
 	//fill in the calendar header
 	$('#month_year').html(getMonthName() + " " + getYear());
@@ -104,21 +158,35 @@ var do_on_load = function(){
 		defaultTime: false
 	});
 }
-$(document).ready(do_on_load);
-$(window).bind('page:change', do_on_load);
+$(document).ready(do_on_load);					
+$(window).bind('page:change', do_on_load);	//not quite sure what this does? Does it prevent the "refresh" issue?
 
 $(document).on('click', '#backMonth', function(){
-	alert("backMonth");
+	decrementMonth();
+	clearMonth();
+	$('#month_year').html(getMonthName() + " " + getYear());
+	makeMonth();
 });	
 
 $(document).on('click', '#backYear', function(){
-	alert("backYear");
+	decrementYear();
+	clearMonth();
+	$('#month_year').html(getMonthName() + " " + getYear());
+	makeMonth();
 });	
 
 $(document).on('click', '#forwardMonth', function(){
-	alert("forwardMonth");
+	//alert("forwardMonth");
+	incrementMonth();
+	clearMonth();
+	$('#month_year').html(getMonthName() + " " + getYear());
+	makeMonth();
+
 });	
 
 $(document).on('click', '#forwardYear', function(){
-	alert("forwardYear");
+	incrementYear();
+	clearMonth();
+	$('#month_year').html(getMonthName() + " " + getYear());
+	makeMonth();
 });	
